5937c146fbc010f7487f60a37f91815c05ba25fb
varo1990 <varopspikyan@gmail.com>
2023-01-08 17:08:18 +0300

    initial version

---
+++ js/script.js
@@ -0,0 +1,17 @@
+AOS.init();
+$('.header_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
